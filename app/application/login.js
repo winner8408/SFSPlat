2bbaa6e65fe3e79521dc146b08745f2107c49932
lihubadboy <lihubadboy@163.com>
2018-03-30 18:02:25 +0800

    提交文档

--- app/application/login.js
+++ app/application/login.js
@@ -2,90 +2,45 @@ define('application/login', ['utils/ajaxUtil', 'utils/common'], function(ajaxUti
     var Widget = function(options) {
         var _self = this;
         _self.options = options;
-        _self._constructPaceLoader();
         _self.ajaxUtil = new ajaxUtil(_self.options.proxyUrl);
         _self.common = new common();
-        _self._getAuthorInfo();
         _self._init();
     };
     Widget.prototype = {
         _init: function() {
             var _self = this;
-            _self._event();
+            _self._sendCode();
+            _self._register();
         },
-        _constructPaceLoader: function() {
-            var paceOptions = {
-                ajax: false, // disabled
-                document: false, // disabled
-                eventLag: false, // disabled
-                elements: {
-                    selectors: ['.my-page']
+        _sendCode:function(){
+            var _self = this;
+            $('#butLogin').on('click',function(){
+                var user = {
+                    username : $('#username').val(),
+                    password :  $('#password').val()
                 }
-            };
-        },
-        _getAuthorInfo: function() {
-            var _self = this;
-            var cookies = _self.common.getCookieValue(_self.options.authorInfoKey);
-            if (cookies == "" || cookies == null || cookies == undefined) {
-                $('.login-form').removeClass('hide');
-            } else {
-                $('.login-container .list-inline').removeClass('hide').addClass('animated bounceIn');
-            }
-        },
-        _formartUrl: function(url) {
-            var _self = this;
-            return url.indexOf(window.location.host) > -1 ? url : _self.options.proxyUrl + '?' + url;
-        },
-        _event: function() {
-            var _self = this;
-
-            $('#index').on('click', function() { window.location = 'index.html'; });
-            $('#administrative').on('click', function() { window.location = 'administrative.html'; });
-            $('#public').on('click', function() { window.open(_self.options.publicUrl); });
-            $(".btn").on("click", function() {
-                var username = $("#username").val();
-                var password = $("#password").val();
-                if (username == "" || password == "") {
-                    $("#messages").html("<font style='color: red'>用户名或密码为空，请重新输入！</font>");
-                    return false;
-                }
-
                 try {
                     $.ajax({
-                        type: "GET",
-                        url: _self._formartUrl(_self.options.LoginUrls.url + "?username=" + username + "&password=" + password),
+                        type: "POST",
+                        url: _self.options.OprUrls.security.sendCode + mobile,
                         dataType: "json",
-                        contentType: "application/json",
+                        contentType: "application/x-www-form-urlencoded",
+                        Authorization :"",
                         timeout: 2000,
                         success: function(data, status, xhr) {
-                            if (data.data) {
-                                var author = {
-                                    userid: data.data.id,
-                                    username: data.data.alias,
-                                    localBureau: data.data.localBureau.name
-                                }
-                                _self.common.setCookie(_self.options.authorInfoKey, JSON.stringify(author), '', '/');
-                                $("#messages").html("");
+                            if (data) {
                                 window.location.href = "index.html";
                             } else {
-                                $("#messages").html("<font style='color: red'>" + data.msg + "！</font>");
-                                $("#username").focus();
-                                $(".btn-login").html("登入");
-                                return false;
                             }
                         },
                         error: function(xhr, error, exception) {
-                            $("#messages").html("<font style='color: red'>登录失败！</font>");
-                            $(".btn-login").html("登入");
+
                         }
                     });
                 } catch (e) {
-                    $("#messages").html("<font style='color: red'>登录异常！</font>");
-                    $(".btn-login").html("登入");
                 }
             });
-        }
-
+        },
     };
 
     return Widget;
--- app/application/register.js
+++ app/application/register.js
@@ -15,10 +15,11 @@ define('application/register', ['utils/ajaxUtil', 'utils/common'], function(ajax
         _sendCode:function(){
             var _self = this;
             $('#sendcode').on('click',function(){
+                var mobile = $('#registerMobile').val();
                 try {
                     $.ajax({
                         type: "GET",
-                        url: _self.options.OprUrls.security.sendCode + '13011174430',
+                        url: _self.options.OprUrls.security.sendCode + mobile,
                         dataType: "json",
                         contentType: "application/json",
                         timeout: 2000,
@@ -26,7 +27,6 @@ define('application/register', ['utils/ajaxUtil', 'utils/common'], function(ajax
                             if (data) {
                                 window.location.href = "index.html";
                             } else {
-
                             }
                         },
                         error: function(xhr, error, exception) {
@@ -34,95 +34,44 @@ define('application/register', ['utils/ajaxUtil', 'utils/common'], function(ajax
                         }
                     });
                 } catch (e) {
-                    $("#messages").html("<font style='color: red'>登录异常！</font>");
-                    $(".btn-login").html("登入");
                 }
             });
         },
         _register:function(){
             var _self = this;
-            $('#sendcode').on('click',function(){
+            $('#butRegister').on('click',function(){
                 var user = {
                     name : $('#registerName').val(),
                     fullname: $('#registerName').val(),
                     mobile: $('#registerMobile').val(),
                     password: $('#registerPassword').val()
                 }
-            })
-        },
-        _constructPaceLoader: function() {
-            var paceOptions = {
-                ajax: false, // disabled
-                document: false, // disabled
-                eventLag: false, // disabled
-                elements: {
-                    selectors: ['.my-page']
-                }
-            };
-        },
-        _getAuthorInfo: function() {
-            var _self = this;
-            var cookies = _self.common.getCookieValue(_self.options.authorInfoKey);
-            if (cookies == "" || cookies == null || cookies == undefined) {
-                $('.login-form').removeClass('hide');
-            } else {
-                $('.login-container .list-inline').removeClass('hide').addClass('animated bounceIn');
-            }
-        },
-        _formartUrl: function(url) {
-            var _self = this;
-            return url.indexOf(window.location.host) > -1 ? url : _self.options.proxyUrl + '?' + url;
-        },
-        _event: function() {
-            var _self = this;
-
-            $('#index').on('click', function() { window.location = 'index.html'; });
-            $('#administrative').on('click', function() { window.location = 'administrative.html'; });
-            $('#public').on('click', function() { window.open(_self.options.publicUrl); });
-            $(".btn").on("click", function() {
-                var username = $("#username").val();
-                var password = $("#password").val();
-                if (username == "" || password == "") {
-                    $("#messages").html("<font style='color: red'>用户名或密码为空，请重新输入！</font>");
-                    return false;
-                }
-
+                var smsCode= $('#registerCode').val();
                 try {
                     $.ajax({
-                        type: "GET",
-                        url: _self._formartUrl(_self.options.LoginUrls.url + "?username=" + username + "&password=" + password),
+                        type: "POST",
+                        url: _self.options.OprUrls.security.register + '?mobile=' + user.mobile + '&smscode=' + smsCode,
+                        data: user,
                         dataType: "json",
                         contentType: "application/json",
-                        timeout: 2000,
+                        timeout: 30000,
                         success: function(data, status, xhr) {
-                            if (data.data) {
-                                var author = {
-                                    userid: data.data.id,
-                                    username: data.data.alias,
-                                    localBureau: data.data.localBureau.name
-                                }
-                                _self.common.setCookie(_self.options.authorInfoKey, JSON.stringify(author), '', '/');
-                                $("#messages").html("");
-                                window.location.href = "index.html";
-                            } else {
-                                $("#messages").html("<font style='color: red'>" + data.msg + "！</font>");
-                                $("#username").focus();
-                                $(".btn-login").html("登入");
-                                return false;
+                            if (data.error_code) {
+                                if (data.error_code.indexOf('401') >= 0)
+                                    return; // gotologin
                             }
+                            if (callback) callback(data);
                         },
                         error: function(xhr, error, exception) {
-                            $("#messages").html("<font style='color: red'>登录失败！</font>");
-                            $(".btn-login").html("登入");
+                            if (callback) callback(null);
                         }
                     });
-                } catch (e) {
-                    $("#messages").html("<font style='color: red'>登录异常！</font>");
-                    $(".btn-login").html("登入");
+                } catch (e) { //Here should be delaying with error
+                    if (callback) callback(null);
                 }
-            });
-        }
-
+                
+            })
+        },
     };
 
     return Widget;
